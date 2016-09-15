0a221819d5b21caf689e2220868d2d65e7415d1e
David Fortin <dafortin@ciena.com>
2016-09-15 17:20:38 -0400

    Rename ember-frost-lts to ember-cli-ecosystem-installer

--- blueprints/ember-cli-ecosystem-installer/index.js
+++ blueprints/ember-cli-ecosystem-installer/index.js
@@ -18,13 +18,13 @@ var display = require('../../lib/ui/display')
 
 var MESSAGES = {
   QUESTION_RECOMMENDED_GROUPS:
-    'Choose the LTS features to install [' + figures.radioOn + ' ] or uninstall [' + figures.radioOff + ' ]',
+    'Choose the ecosystem features to install [' + figures.radioOn + ' ] or uninstall [' + figures.radioOff + ' ]',
   QUESTION_OTHER_GROUPS:
     'Choose the application specific packages to keep [' + figures.radioOn +
     ' ] or uninstall [' + figures.radioOff + ' ]',
-  INSTALLING_LTS: 'Installing LTS',
-  LOADING_VALIDATING_LTS_FILES: 'Loading and validating LTS files',
-  LOADED_VALIDATED_LTS_FILES: 'Loaded and validated LTS files\n',
+  INSTALLING_LTS: 'Installing ecosystem features',
+  LOADING_VALIDATING_LTS_FILES: 'Loading and validating ecosystem files',
+  LOADED_VALIDATED_LTS_FILES: 'Loaded and validated ecosystem files\n',
   CONFIRMATION: 'Would you like to confirm the following choices',
   SUMMARY: 'Summary of the operations that will be done',
   UNINSTALLING_PKGS: 'Uninstalling packages',
@@ -115,7 +115,7 @@ module.exports = {
     console.log('|    |   |    |  \\_____  \\ ')
     console.log('|    |___|    |  /        \\ ')
     console.log('|________\\____| /_________/ ')
-    console.log('Tool to install/uninstall LTS packages\n')
+    console.log('Tool to install/uninstall the LTS ecosystem packages\n')
   },
 
   /**
